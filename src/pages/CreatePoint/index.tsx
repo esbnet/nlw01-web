0df86068322c5522b0fd33f5cc7783698dcfd12b
Edmilson Soares <esbnet@gmail.com>
2020-06-05 23:36:10 -0300

    Mapa (Leaflet) integrado ao form

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiArrowLeft } from 'react-icons/fi'
+import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './style.css';
 
@@ -59,6 +60,14 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione um endereço no mapa</span>
           </legend>
+
+          <Map center={[-12.9330657, -38.419196]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[-12.9330657, -38.419196]} />
+          </Map>
 
           <div className="field-group">
             <div className="field">
